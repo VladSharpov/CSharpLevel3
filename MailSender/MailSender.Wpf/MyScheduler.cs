e44ae642dd7836fd1fc2cbdfd1627a92b929d020
VladSharpov <vku3228@gmail.com>
2019-04-08 01:53:30 +0300

    Homework 2

---
+++ MailSender/MailSender.Wpf/MyScheduler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace MailSender.Wpf
+{
+    /// <summary>
+    /// Класс-планировщик, который создает расписание, следит за его выполнением и напоминает о событиях
+    /// Также помогает автоматизировать рассылку писем в соответствии с расписанием
+    /// </summary>
+    class MyScheduler
+    {
+        DispatcherTimer timer = new DispatcherTimer(); // таймер 
+        EmailSendServiceClass emailSender;         // экземпляр класса, отвечающего за отправку писем
+        DateTime dateTimeSend;                           // дата и время отправки
+        IQueryable<Email> emails;                  // коллекция email-ов адресатов
+
+        /// <summary>
+        /// Метод, который превращает строку из текстбокса textBoxTimePicker в TimeSpan
+        /// </summary>
+        /// <param name="strSendTime"></param>
+        /// <returns></returns>
+        public TimeSpan GetSendTime(string strSendTime)
+        {
+            TimeSpan tsSendTime = new TimeSpan();
+
+            try
+            {
+                tsSendTime = TimeSpan.Parse(strSendTime);
+            }
+            catch { }
+
+            return tsSendTime;
+        }
+
+        /// <summary>
+        //// Непосредственно отправка писем
+        /// </summary>
+        /// <param name="dtSend"></param>
+        /// <param name="emailSender"></param>
+        /// <param name="emails"></param>
+        public void SendEmails(DateTime dateTimeSend, EmailSendServiceClass emailSender, IQueryable<Email> emails)
+        {
+            this.emailSender = emailSender; // Экземпляр класса, отвечающего за отправку писем, присваиваем 
+            this.dateTimeSend = dateTimeSend;
+            this.emails = emails;
+
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (dateTimeSend.ToShortTimeString() == DateTime.Now.ToShortTimeString())
+            {
+                emailSender.SendMails(emails);
+                timer.Stop();
+                MessageBox.Show("Mails sent", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
---
+++ MailSender/TabSwitcher/TabSwitcherControl.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TabSwitcher
+{
+    /// <summary>
+    /// Interaction logic for TabSwitcherControl.xaml
+    /// </summary>
+    public partial class TabSwitcherControl : UserControl
+    {
+        public event RoutedEventHandler ButtonNextClick;
+        public event RoutedEventHandler ButtonPreviousClick;
+
+        public TabSwitcherControl()
+        {
+            InitializeComponent();
+        }
+
+        #region Properties
+
+        private bool previousIsHidden = false; // поле, соответствующее тому, будет ли скрыта кнопка «Предыдущий»
+        private bool nextIsHidden = false; // поле, соответствующее тому, будет ли скрыта кнопка «Следующий»
+
+        /// <summary>
+        /// Свойство, соответствующее тому, будет ли скрыта кнопка «Предыдущий». 
+        /// Чтобы Свойство отразилось на PropertiesGrid у нашего контрола, его нужно представить именно свойством, а не полем
+        /// </summary>
+        public bool PreviousIsHidden
+        {
+            get => previousIsHidden;
+            set
+            {
+                previousIsHidden = value;
+                SetButtons(); // метод, который отвечает на отрисовку кнопок
+            }
+        }
+
+        public bool NextIsHidden
+        {
+            get => nextIsHidden;
+            set
+            {
+                nextIsHidden = value;
+                SetButtons(); // метод, который отвечает за отрисовку кнопок
+            }
+        }
+
+        private void SetPreviousOffNextOn()
+        {
+            buttonNext.Visibility = Visibility.Hidden;
+            buttonPrevious.Visibility = Visibility.Visible;
+            buttonPrevious.Width = 150;
+            buttonNext.Width = 0;
+            buttonPrevious.HorizontalAlignment = HorizontalAlignment.Stretch;
+        }
+
+        private void SetPreviousOnNextOff()
+        {
+            buttonPrevious.Visibility = Visibility.Hidden;
+            buttonNext.Visibility = Visibility.Visible;
+            buttonNext.Width = 150;
+            buttonPrevious.Width = 0;
+            buttonNext.HorizontalAlignment = HorizontalAlignment.Stretch;
+        }
+
+        private void SetPreviousOffNextOff()
+        {
+            buttonNext.Visibility = Visibility.Visible;
+            buttonPrevious.Visibility = Visibility.Visible;
+            buttonNext.Width = 75;
+            buttonPrevious.Width = 75;
+        }
+
+        private void SetPreviousOnNextOn()
+        {
+            buttonPrevious.Visibility = Visibility.Hidden;
+            buttonNext.Visibility = Visibility.Hidden;
+        }
+
+        /// <summary>
+        /// Метод, который отвечает за отрисовку кнопок.
+        /// Есть три варианта: когда обе кнопки доступны; доступна одна и недоступна вторая; обе кнопки недоступны
+        /// </summary>
+        private void SetButtons()
+        {
+            if (previousIsHidden && nextIsHidden)
+            {
+                SetPreviousOnNextOn();
+            }
+            else if (!nextIsHidden && !previousIsHidden)
+            {
+                SetPreviousOffNextOff();
+            }
+            else if (nextIsHidden && !previousIsHidden)
+            {
+                SetPreviousOffNextOn();
+            }
+            else if (!nextIsHidden && previousIsHidden)
+            {
+                SetPreviousOnNextOff();
+            }
+        }
+
+
+        #endregion
+
+        private void ButtonNext_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonNextClick?.Invoke(sender, e);
+        }
+
+        private void ButtonPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPreviousClick?.Invoke(sender, e);
+        }
+    }
+}
